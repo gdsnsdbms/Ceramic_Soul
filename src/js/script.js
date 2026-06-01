e29178f5201f6bbef9094ce2732a64c75cae4302
ira <ivanovaid1706@gmail.com>
2026-06-01 18:16:00 +0300

    added adaptation to the works section

--- src/js/script.js
+++ src/js/script.js
@@ -7,5 +7,26 @@ import "swiper/css/pagination";
 import "/src/sass/style.scss";
 
 const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".icon-right-open",
+        prevEl: ".icon-left-open",
+    },
+    breakpoints: {
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5,
+        },
+        1920: {
+            slidesPerView: 3,
+            spaceBetween: 35,
+        },
+    },
     modules: [Navigation, Pagination],
 });
