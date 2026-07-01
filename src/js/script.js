0d6ff9ce62765d8fb24b3c80cee21feda56aea3f
ira <ivanovaid1706@gmail.com>
2026-07-01 17:10:12 +0300

    added animations to the promo and touch sections

--- src/js/script.js
+++ src/js/script.js
@@ -123,7 +123,21 @@ try {
                         ".checkbox-error-message",
                     ),
             },
-        );
+        )
+        .onSuccess((event) => {
+            const form = event.currentTarget;
+            const formData = new FormData(form);
+
+            fetch("https://jsonplaceholder.typicode.com/posts", {
+                method: "POST",
+                body: formData,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("Success", data);
+                    form.reset();
+                });
+        });
 } catch (e) {}
 
 try {
